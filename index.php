f3636b734979286b5d11681f96c1dbcc9c6edba0
louiscassidy <louiscassidy@users.noreply.github.com>
2017-07-01 15:26:22 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
+</head>
+
+<body>
+
+<div class="g-signin2" data-onsuccess="signin.php"></div>
+
+</body>
+</html>
